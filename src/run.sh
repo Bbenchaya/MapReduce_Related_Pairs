8e2d341d7578052730a7d5727312fcae81bf9530
asafch <asaf.chelouche@gmail.com>
2016-06-04 18:26:28 +0300

    switch to CountWords2

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-hadoop fs -rm -r /user/$HADOOP_USERNAME/wordcount/output
+hadoop fs -rm -r /user/$HADOOP_USERNAME/output
 hadoop com.sun.tools.javac.Main WordCount.java
 jar cf wc.jar WordCount*.class
-hadoop jar wc.jar WordCount /user/$HADOOP_USERNAME/wordcount/input /user/$HADOOP_USERNAME/wordcount/output
+hadoop jar wc.jar WordCount -Dwordcount.case.sensitive=false /user/$HADOOP_USERNAME/input /user/$HADOOP_USERNAME/output -skip /user/$HADOOP_USERNAME/patterns.txt
